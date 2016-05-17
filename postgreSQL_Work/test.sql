50a56cfa57382530d1115c30994b90708f93a7d7
Eric Meyers <eric.meyers12@gmail.com>
2016-05-17 10:50:29 -0500

    Initial commit - adding some basic sql scripts

---
+++ postgreSQL_Work/test.sql
@@ -0,0 +1,19 @@
+﻿-- Table: test
+
+-- DROP TABLE test;
+
+CREATE TABLE COMPANY(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL,
+   JOIN_DATE	  DATE
+)
+WITH (
+  OIDS=FALSE
+);
+
+INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY,JOIN_DATE) VALUES (1, 'Paul', 32, 'California', 20000.00 ,'2001-07-13');
+ALTER TABLE test
+  OWNER TO "Eric_Meyers";
